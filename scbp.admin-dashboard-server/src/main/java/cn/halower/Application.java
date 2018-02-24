4a8105be65231ef963ff57ec927381016b354f5e
halower <121625933@qq.com>
2018-02-24 16:47:16 +0800

    配置修改

--- scbp.admin-dashboard-server/src/main/java/cn/halower/Application.java
+++ scbp.admin-dashboard-server/src/main/java/cn/halower/Application.java
@@ -6,9 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@SpringBootApplication
+@EnableEurekaServer
 @EnableCloudDashboard
 @EnableDiscoveryClient
-@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
