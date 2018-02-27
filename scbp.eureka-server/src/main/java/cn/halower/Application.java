4117dcc889dacba9258c7a46767bfd2bb377030b
halower <121625933@qq.com>
2018-02-27 13:36:00 +0800

    Update Config

--- scbp.eureka-server/src/main/java/cn/halower/Application.java
+++ scbp.eureka-server/src/main/java/cn/halower/Application.java
@@ -1,7 +1,7 @@
 package cn.halower;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
@@ -9,6 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class Application {
 
     public static void main(String[]  args) {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
+        SpringApplication.run(Application.class,args);
     }
 }
