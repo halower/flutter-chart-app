778e82f4255760693d35b41feaf80ba453eb7924
DESKTOP-QTQK8VQ\halower <chinahalower@gmail.com>
2018-03-01 00:22:11 +0800

    auto-enable Refresh Endpoint

--- scbp.config-server/src/main/java/cn/halower/ConfigServerApplication.java
+++ scbp.config-server/src/main/java/cn/halower/ConfigServerApplication.java
@@ -2,12 +2,12 @@ package cn.halower;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
-@EnableConfigServer
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication{
     public static void main(String[] args) {
         new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
--- scbp.service-providers/scbp.api-confclientdemo-service/src/main/java/cn/halower/ConfigClientApplication.java
+++ scbp.service-providers/scbp.api-confclientdemo-service/src/main/java/cn/halower/ConfigClientApplication.java
@@ -4,13 +4,13 @@ package cn.halower;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @RestController
 @RefreshScope
 public class ConfigClientApplication {
