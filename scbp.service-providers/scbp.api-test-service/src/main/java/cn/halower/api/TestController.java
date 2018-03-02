b9ee48abb85b181fb9c899e000218e1ce1a0401f
halower <121625933@qq.com>
2018-03-02 13:57:15 +0800

    zipkin -mysql

--- scbp.service-providers/scbp.api-test-service/src/main/java/cn/halower/api/TestController.java
+++ scbp.service-providers/scbp.api-test-service/src/main/java/cn/halower/api/TestController.java
@@ -4,12 +4,10 @@ import cn.halower.feigns.TestFeignClient;
 import cn.halower.models.Demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope
 public class TestController {
     @Autowired
     private DiscoveryClient discoveryClient;
