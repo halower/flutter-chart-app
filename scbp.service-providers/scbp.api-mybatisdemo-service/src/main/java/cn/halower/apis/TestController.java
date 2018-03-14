5702dd8cf7db0b7b899e86df0adac9f8ae46407f
DESKTOP-QTQK8VQ\halower <chinahalower@gmail.com>
2018-03-14 18:29:45 +0800

    添加mybatis示例

---
+++ scbp.service-providers/scbp.api-mybatisdemo-service/src/main/java/cn/halower/apis/TestController.java
@@ -0,0 +1,18 @@
+package cn.halower.apis;
+
+import cn.halower.dao.UserMapper;
+import cn.halower.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @Autowired
+    public UserMapper _userMappers;
+    @GetMapping("/hello")
+    public String Get() {
+        User user =  _userMappers.selectByUserId(1);
+        return user.getName();
+    }
+}
