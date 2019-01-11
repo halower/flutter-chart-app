1de71ad221bbdc2664f7ce5093915785985ff530
halower <121625933@qq.com>
2019-01-11 11:54:26 +0800

    feat:apollo demo: add a demo for springcloud with apollo

--- newbie-spring-cloud-examples-configclient/src/main/java/newbie/configclient/ConfigClientApplication.java
+++ newbie-spring-cloud-examples-configclient/src/main/java/newbie/configclient/ConfigClientApplication.java
@@ -3,11 +3,21 @@ package newbie.configclient;
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.GET;
 
 @EnableApolloConfig
+@RestController
 @SpringBootApplication
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class,args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello,World";
     }
 }
